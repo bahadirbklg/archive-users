0a7226053a48350bfd647e50e0f058876d1f5cbc
bbahadir <bahadir.bakkaloglu@stu.istinye.edu.tr>
2021-10-13 02:47:39 +0300

    tearDownClass method used
    tearDownClass seems more appropriate because setUp and tearDown method launches before and after every test which is unnecessary

--- test_main.py
+++ test_main.py
@@ -3,23 +3,31 @@ import pathlib
 import os
 import subprocess
 import shutil
+from unittest.loader import TestLoader
 
 class TestMain(unittest.TestCase):
 
-    def setUp(self) -> None:
-        
-
-        #this line makes code compatible only with linux
-        subprocess.run(['sudo', 'groupadd',"testgroup"]) 
+    @classmethod
+    def setUpClass(cls):
+        #subprocess line makes code compatible only with linux
+        subprocess.run(['sudo', 'groupadd',"testgroup"])
+        subprocess.run(['sudo', 'useradd',"testuser"])
         pathlib.Path.mkdir('testdir')
         pathlib.Path.touch('testdir/testfile.txt')
+        subprocess.run(['sudo', 
+                        'chown','-R',
+                        "testuser:testgroup",'testdir'])
         return super().setUp()
 
-    def tearDown(self) -> None:
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree('testdir/')
+
+        subprocess.run(['sudo', 'chown','-R',"testgroup"])
         subprocess.run(['sudo', 'groupdel',"testgroup"])
-        shutil.rmtree('testdir/testfile.txt')
+        subprocess.run(['sudo', 'userdel',"testuser"])
         return super().tearDown()
 
     def test_findFilesByGroup(self):
-        self.assertEqual()
+        self.assertEquals()
         pass
